894287f9289669f1b2f6dc601ba3edd2af1a9e66
Helio Rabelo <heliobrunorabelo@gmail.com>
2021-03-04 16:10:05 -0300

    Desafio finalizado.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,16 @@
 import Transaction from '../models/Transaction';
 
+interface Transactions {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -13,16 +24,51 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): Transactions {
+    const transactions = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acumulador, valorAtual) => {
+      if (valorAtual.type === 'income') {
+        return acumulador + valorAtual.value;
+      }
+      return acumulador;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acumulador, valorAtual) => {
+      if (valorAtual.type === 'outcome') {
+        return acumulador + valorAtual.value;
+      }
+      return acumulador;
+    }, 0);
+
+    const total = income - outcome;
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (transaction.type === 'outcome') {
+      const saldo = this.getBalance().total;
+      if (saldo - transaction.value < 0) {
+        throw Error('The desired amount exceeds the account limit');
+      }
+    }
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
